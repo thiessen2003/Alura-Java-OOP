acb98863f167259a6de2b64b4c59343878dbaa72
thiessen2003 <thiessengabriel@gmail.com>
2022-09-04 19:22:15 -0300

    Daily Commit

---
+++ sintaxeBasica/TestaPontoFlutuante.java
@@ -0,0 +1,13 @@
+package sintaxeBasica;
+
+public class TestaPontoFlutuante {
+
+    public static void main(String[] args) {
+
+        double salario;
+        salario = 1250.70;
+        System.out.println(salario);
+
+    }
+
+}
---
+++ sintaxeBasica/TestaVariaveis.java
@@ -0,0 +1,21 @@
+package sintaxeBasica;
+
+public class TestaVariaveis {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá novo teste");
+
+        int idade;
+        idade = 37;
+
+        System.out.println(idade);
+
+        idade = 30 + 10;
+
+        System.out.println("a idade é" + idade);
+
+
+    }
+
+}
