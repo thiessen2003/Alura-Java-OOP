5270314aec1828a29a56370b84b64ddc227e08be
thiessen2003 <thiessengabriel@gmail.com>
2022-09-05 11:51:28 -0300

    Daily Commit

---
+++ polimorfismo/bytebankHerdado/Funcionario.java
@@ -0,0 +1,38 @@
+package polimorfismo.bytebankHerdado;
+
+public class Funcionario {
+
+    private String nome;
+    private String cpf;
+    private double salario;
+
+
+
+    public double getBonificacao() {
+        return this.salario * 0.1;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
---
+++ polimorfismo/bytebankHerdado/TesteFuncionario.java
@@ -0,0 +1,14 @@
+package polimorfismo.bytebankHerdado;
+
+public class TesteFuncionario {
+
+    public static void main(String[] args) {
+
+        Funcionario joao = new Funcionario();
+        joao.setSalario(2589.08);
+        joao.setNome("João Santos");
+        joao.setCpf("012-322-112-10");
+
+    }
+
+}
