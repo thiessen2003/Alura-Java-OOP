98ee104685a8fc3b2b0e3aab1c1ec47c7a7b1b63
thiessen2003 <thiessengabriel@gmail.com>
2022-09-05 07:39:42 -0300

    Daily Commit

---
+++ bytebank/TestaMetodo.java
@@ -0,0 +1,18 @@
+package bytebank;
+
+public class TestaMetodo {
+
+    public static void main(String[] args) {
+        Conta minhaConta = new Conta();
+        minhaConta.deposita(300);
+        System.out.println(minhaConta.saldo);
+        minhaConta.saca(20);
+        System.out.println(minhaConta.saldo);
+
+        Conta outraConta = new Conta();
+        outraConta.deposita(1500);
+
+        outraConta.transfere(200, minhaConta)
+    }
+
+}
