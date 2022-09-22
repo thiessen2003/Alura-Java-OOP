1a95b941ff80dbdf6199835bba895de70c094983
thiessen2003 <thiessengabriel@gmail.com>
2022-09-21 22:53:39 -0300

    Daily Commit

---
+++ bytebankConta/teste/TesteString.java
@@ -0,0 +1,15 @@
+package bytebankConta.teste;
+
+public class TesteString {
+
+    public static void main(String[] args) {
+        String nome = "Alura";
+        //strings são imutáveis
+        String outra = nome.replace("A", "a");
+
+
+        System.out.println(nome);
+        System.out.println(outra);
+    }
+
+}
