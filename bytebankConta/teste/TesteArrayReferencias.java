7862398d478320cb02183ed3ad7585fa3fff2d15
thiessen2003 <thiessengabriel@gmail.com>
2022-10-05 09:19:22 -0300

    Daily Commit

--- bytebankConta/teste/TesteArrayReferencias.java
+++ bytebankConta/teste/TesteArrayReferencias.java
@@ -2,17 +2,21 @@ package bytebankConta.teste;
 
 import bytebankConta.modelo.Conta;
 import bytebankConta.modelo.ContaCorrente;
+import bytebankConta.modelo.ContaPoupanca;
 import bytebankConta.teste.TesteContas;
 
 public class TesteArrayReferencias {
 
     public static void main(String[] args) {
         ContaCorrente[] contas = new ContaCorrente[5];
+        ContaPoupanca[] cp2 = new ContaPoupanca[3];
+
 
         Conta[] contas2 = new Conta[2];
 
         ContaCorrente cc1 = new ContaCorrente(22, 11);
         ContaCorrente cc2 = new ContaCorrente(53, 12);
+
 
         contas[0] = cc1;
         contas[1] = cc2;
